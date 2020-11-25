d4a1bfc047cb0204eed755d6c4ee870724f15882
JesusArtiles <jesusartilescidoncha@gamil.com>
2020-11-25 22:11:46 +0000

    Add: added exception for negatives numbers and possibility to chose default delimiter

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -2,13 +2,21 @@ package StringCalculator;
 
 public class StringCalculator {
 
-    public int add(String numbers){
-        if(numbers.length() == 0) return 0;
+    private String BASIC_REGEX = "[,\n]";
 
-        String regex = "[,\n]";
+    public int add(String numbers) throws Exception {
+        if(numbers.length() == 0) return 0;
+        if(numbers.split(BASIC_REGEX).length == 1){
+            if(Integer.parseInt(numbers) < 0) throw new Exception("negatives not allowed");
+            return Integer.parseInt(numbers);
+        }
+
+        String[] regexAndNumbers = getRegexAndNumbers(numbers);
+        numbers = regexAndNumbers[1];
+        String regex = regexAndNumbers[0];
 
         String[] parts = numbers.split(regex);
-        if(parts.length == 1) {
+        if (parts.length == 1) {
             return Integer.parseInt(numbers);
         } else if (parts.length > 1) {
             return getSum(parts);
@@ -17,12 +25,29 @@ public class StringCalculator {
         return 0;
     }
 
-    private int getSum(String[] parts) {
+    private int getSum(String[] parts) throws Exception {
         int sumResult = 0;
 
         for(String s: parts) {
+            if(Integer.parseInt(s) < 0) throw new Exception("negatives not allowed");
             sumResult += Integer.parseInt(s);
         }
+
         return sumResult;
+    }
+
+    private String[] getRegexAndNumbers(String numbers){
+        String[] result = new String[2];
+
+         if(numbers.startsWith("//")){
+            String[] parts = numbers.substring(2).split("\n");
+            result[0] = parts[0];
+            result[1] = parts[1];
+        } else {
+            result[0] = BASIC_REGEX;
+            result[1] = numbers;
+        }
+
+        return result;
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,6 +1,8 @@
 import StringCalculator.StringCalculator;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,19 +12,19 @@ public class StringCalculatorShould {
     private StringCalculator calculator = new StringCalculator();
 
     @Test
-    public void return_0_when_given_String_is_empty(){
+    public void return_0_when_given_String_is_empty() throws Exception {
         assertThat(calculator.add("")).isEqualTo(0);
     }
 
     @Test
-    public void return_number_when_given_only_one_number(){
+    public void return_number_when_given_only_one_number() throws Exception {
         assertThat(calculator.add("0")).isEqualTo(0);
         assertThat(calculator.add("2")).isEqualTo(2);
         assertThat(calculator.add("10")).isEqualTo(10);
     }
 
     @Test
-    public void return_sum_of_numbers_when_given_2_numbers(){
+    public void return_sum_of_numbers_when_given_2_numbers() throws Exception {
         assertThat((calculator.add("1,1"))).isEqualTo(2);
         assertThat((calculator.add("1,2"))).isEqualTo(3);
         assertThat((calculator.add("2,3"))).isEqualTo(5);
@@ -31,7 +33,7 @@ public class StringCalculatorShould {
     }
 
     @Test
-    public void return_sum_of_numbers_when_given_unknown_number_of_numbers(){
+    public void return_sum_of_numbers_when_given_unknown_number_of_numbers() throws Exception {
         assertThat((calculator.add("1,1,1"))).isEqualTo(3);
         assertThat((calculator.add("1,2,5,10"))).isEqualTo(18);
         assertThat((calculator.add("2,30,2,3,4,5"))).isEqualTo(46);
@@ -40,5 +42,23 @@ public class StringCalculatorShould {
         assertThat((calculator.add("3\n1,1"))).isEqualTo(5);
         assertThat((calculator.add("1\n8\n1,3,1"))).isEqualTo(14);
     }
-    
+
+    @Test
+    public void return_sum_of_numbers_when_given_string_with_delimiter_change() throws Exception {
+        assertThat((calculator.add("//;\n1;2"))).isEqualTo(3);
+        assertThat((calculator.add("//@\n1@2@3"))).isEqualTo(6);
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void throw_exception_when_given_negative_number() throws Exception {
+        exceptionRule.expect(Exception.class);
+        exceptionRule.expectMessage("negatives not allowed");
+        calculator.add("-1");
+        calculator.add("1,2,5,-10");
+        calculator.add("//;\n1;-2");
+    }
+
 }
