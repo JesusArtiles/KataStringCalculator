7d903bf2108e0ce886c1b22f476f12da99eb4496
JesusArtiles <jesusartilescidoncha@gamil.com>
2020-11-26 22:02:17 +0000

    Add: Now it allows multiple delimiters of multiple lengths and performed general refactor

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -4,86 +4,105 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import Exception.NegativeNumberException;
 
 public class StringCalculator {
 
-    private String BASIC_REGEX = "[,\n]";
+    private final String BASIC_REGEX = "[,\n]";
 
-    public int add(String numbers) throws Exception {
-        if(numbers.length() == 0) return 0;
-        if(numbers.split(BASIC_REGEX).length == 1 && !(numbers.startsWith("//"))){
-            if(Integer.parseInt(numbers) < 0) {
-                throw new Exception("negatives not allowed");
-            }else if (Integer.parseInt(numbers) > 1000){
+    public int add(String numbers) throws NegativeNumberException {
+        boolean numbersOnlyContainsOneNumber = numbers.split(BASIC_REGEX).length == 1 && !(numbers.startsWith("//"));
+        boolean numbersIsEmpty = numbers.length() == 0;
+
+        if (numbersIsEmpty) {
+            return 0;
+        }else if (numbersOnlyContainsOneNumber) {
+            if(numberIsNegative(numbers)) {
+                throw new NegativeNumberException("negatives not allowed");
+            }else if (numberIsGreaterThan1000(numbers)){
                 return 0;
             }
             return Integer.parseInt(numbers);
         }
-        String[] newNumbers = getNumbers(numbers);
-        return performSum(newNumbers);
-
+        String[] numbersToAdd = getNumbers(numbers);
+        return performSum(numbersToAdd);
     }
 
-    private int performSum(String[] parts) throws Exception {
-        int sumResult = 0;
+    private int performSum(String[] numbers) throws NegativeNumberException {
+        int result = 0;
 
-        for(String s: parts) {
-            if(Integer.parseInt(s) < 0) {
-                throw new Exception("negatives not allowed");
-            } else if(Integer.parseInt(s) <= 1000) {
-                sumResult += Integer.parseInt(s);
+        for(String number: numbers) {
+            if(numberIsNegative(number)) {
+                throw new NegativeNumberException("negatives not allowed");
+            } else if(!numberIsGreaterThan1000(number)) {
+                result += Integer.parseInt(number);
             }
         }
-
-        return sumResult;
+        return result;
     }
 
-    private String[] getNumbers(String numbers) throws Exception {
-        String[] result = new String[2];
+    private String[] getNumbers(String numbers) {
+        boolean hasCustomDelimiter = numbers.startsWith("//");
 
-         if(numbers.startsWith("//")){
-            String[] parts = numbers.substring(2).split("\n");
-            String[] newNumbers = getNumbersToAdd(parts[1],parts[0]);
-            return newNumbers;
+         if(hasCustomDelimiter) {
+            String[] delimitersAndNumbers = numbers.substring(2).split("\n");
+            String delimiterPart = delimitersAndNumbers[0];
+            String numberPart = delimitersAndNumbers[1];
+            return getListOfNumbersWithRegexAndNumbers(numberPart,delimiterPart);
         } else {
-            String[] parts = getNumbersToAdd(numbers, BASIC_REGEX);
-            return parts;
+             return getListOfNumbersWithRegexAndNumbers(numbers, BASIC_REGEX);
         }
     }
 
-    private String[] getNumbersToAdd(String numbers, String delimiter) throws Exception {
-        if(delimiter.startsWith("[") && delimiter.endsWith("]") && !delimiter.equals(BASIC_REGEX)){
-            delimiter = getDelimiters(delimiter);
+    private String[] getListOfNumbersWithRegexAndNumbers(String numbers, String delimiter) {
+        boolean delimitersAreSeparatedBySquareBrackets = delimiter.startsWith("[")
+                && delimiter.endsWith("]") && !delimiter.equals(BASIC_REGEX);
+
+        String delimiterRegex;
+        if(delimitersAreSeparatedBySquareBrackets){
+            delimiterRegex = getDelimiterRegex(delimiter);
         }else{
-            delimiter = removeDuplicates(delimiter);
+            delimiterRegex = removeDelimiterDuplicates(delimiter);
         }
-        String[] parts = numbers.split(delimiter);
-        List<String> results = new ArrayList<>();
 
-        for(String s:parts){
-            if(!s.isEmpty()) results.add(s);
+        String[] rawNumberString = numbers.split(delimiterRegex);
+        List<String> listOfNumbersToAdd = new ArrayList<>();
+
+        for(String value:rawNumberString){
+            boolean isANumber = !value.isEmpty();
+            if(isANumber) listOfNumbersToAdd.add(value);
         }
-        String result[] = new String[results.size()];
-        return results.toArray(result);
+
+        String[] result = new String[listOfNumbersToAdd.size()];
+        return listOfNumbersToAdd.toArray(result);
     }
 
-    private String getDelimiters(String delimeter){
-        String[] parts = delimeter.split("]");
-        String result = new String();
+    private String getDelimiterRegex(String delimiter){
+        String[] parts = delimiter.split("]");
+        StringBuilder delimiters = new StringBuilder();
+
         for(int i = 0;i < parts.length;i++){
             if(!parts[i].isEmpty()){
-                result += parts[i].substring(1,parts[i].length());
+                delimiters.append(parts[i].substring(1));
             }
         }
-        if(parts.length == 1) return removeDuplicates(result);
-        return "["+ removeDuplicates(result)+"]";
+        boolean onlyOneDelimiter = parts.length == 1;
+        if(onlyOneDelimiter) return removeDelimiterDuplicates(delimiters.toString());
+        return "["+ removeDelimiterDuplicates(delimiters.toString())+"]";
     }
 
-    private String removeDuplicates(String result) {
-        String noDuplicates = Arrays.asList(result.split(""))
+    private String removeDelimiterDuplicates(String result) {
+        return Arrays.asList(result.split(""))
                 .stream()
                 .distinct()
                 .collect(Collectors.joining());
-        return noDuplicates;
+    }
+
+    private boolean numberIsNegative(String number){
+        return Integer.parseInt(number) < 0;
+    }
+
+    private boolean numberIsGreaterThan1000(String number){
+        return Integer.parseInt(number) > 1000;
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -44,12 +44,11 @@ public class StringCalculatorShould {
     }
 
     @Test
-    public void return_sum_of_numbers_when_given_string_with_delimiter_change() throws Exception {
+    public void return_sum_of_numbers_when_given_string_with_custom_delimiter() throws Exception {
         assertThat((calculator.add("//;\n1;2"))).isEqualTo(3);
         assertThat((calculator.add("//@\n1@2@3"))).isEqualTo(6);
-
+        assertThat((calculator.add("//[@]\n1@2@3"))).isEqualTo(6);
         assertThat((calculator.add("//[@@@@@]\n1@@@@2@@@@3"))).isEqualTo(6);
-        assertThat(calculator.add("//[*][%]\n1*2%3")).isEqualTo(6);
     }
 
     @Rule
@@ -62,12 +61,22 @@ public class StringCalculatorShould {
         calculator.add("-1");
         calculator.add("1,2,5,-10");
         calculator.add("//;\n1;-2");
+        calculator.add("//[*][%]\n1*-2%3");
     }
 
     @Test
-    public void return_correct_sum_of_numbers_ignoring_numbers_grater_than_1000() throws Exception {
+    public void return_sum_of_numbers_by_ignoring_numbers_grater_than_1000() throws Exception {
         assertThat((calculator.add("1\n8\n1,1001,1"))).isEqualTo(11);
         assertThat((calculator.add("2,3,10000"))).isEqualTo(5);
         assertThat((calculator.add("//;\n10000;2"))).isEqualTo(2);
+        assertThat(calculator.add("//[*][%]\n1*2%1001")).isEqualTo(3);
+    }
+
+    @Test
+    public void return_sum_of_numbers_when_given_multiple_delimiters_with_length_longer_than_one() throws Exception {
+        assertThat(calculator.add("//[*][%]\n1*2%3")).isEqualTo(6);
+        assertThat(calculator.add("//[*][%][@]\n1*2%3@5*6")).isEqualTo(17);
+        assertThat(calculator.add("//[*][\t]\n1*2\t3*1")).isEqualTo(7);
+        assertThat(calculator.add("//[*][\t][\b]\n1*2\t3*1\b3")).isEqualTo(10);
     }
 }
